81e48b9372ecf4f61742be0fe81362313b1065cb
PavelOfficial <pavelofficial@ya.ru>
2026-01-21 11:39:58 +0300

    Enhance Next.js configuration with Turbopack and Webpack settings for SVG handling, and add @svgr/webpack as a development dependency.

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,40 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  reactStrictMode: true,
+  // Configuration for Turbopack (used during development by default)
+
+  turbopack: {
+    rules: {
+      '*.svg': {
+        loaders: ['@svgr/webpack'],
+        as: '*.js',
+      },
+    },
+  },  
+
+  // Configuration for Webpack (used during production builds by default)
+  webpack(config) {
+    const fileLoaderRule = config.module.rules.find((rule) => rule.test?.test?.('.svg'));
+
+    config.module.rules.push(
+      {
+        ...fileLoaderRule,
+        test: /\.svg$/i,
+        resourceQuery: /url/,
+      },
+      {
+        test: /\.svg$/i,
+        issuer: fileLoaderRule.issuer,
+        resourceQuery: { not: [...fileLoaderRule.resourceQuery.not, /url/] },
+        use: ['@svgr/webpack'],
+      },
+    );
+
+    fileLoaderRule.exclude = /\.svg$/i;
+
+    return config;
+  },
 };
 
 export default nextConfig;
