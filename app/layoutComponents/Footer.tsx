0638db607de7426a9cce21784099be981aac67f5
PavelOfficial <pavelofficial@ya.ru>
2026-01-21 12:34:44 +0300

    Refactor global styles and update layout structure to include a wrapper for children components

---
+++ app/layoutComponents/Footer.tsx
@@ -0,0 +1,8 @@
+import { JSX } from "react"
+import styles from "./Footer.module.css"
+
+export default Footer = (): JSX.Element  => {
+    return <footer className={styles.footer}>
+
+    </footer>
+}
