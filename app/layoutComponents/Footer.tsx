91b51532ea0112cb7ebc5a699cb8398ff65b65c7
PavelOfficial <pavelofficial@ya.ru>
2026-01-21 13:16:01 +0300

    Implement footer component with styling and update layout to use grid display

--- app/layoutComponents/Footer.tsx
+++ app/layoutComponents/Footer.tsx
@@ -1,8 +1,18 @@
 import { JSX } from "react"
 import styles from "./Footer.module.css"
 
-export default Footer = (): JSX.Element  => {
-    return <footer className={styles.footer}>
-
-    </footer>
+const Footer = (): JSX.Element  => {
+    return (
+        <footer className={styles.footer}>
+            <div className={styles.copyright}>
+                OwlTop © 2020 - 2021 Все права защищены
+            </div>
+            <ul className={styles.userRules}>
+                <li><a href="#" target='_blank'>Пользовательское соглашение</a></li>
+                <li><a href="#" target='_blank'>Политика конфиденциальности</a></li>
+            </ul>
+        </footer>
+    );
 }
+
+export default Footer;
